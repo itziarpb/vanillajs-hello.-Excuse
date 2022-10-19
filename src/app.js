6aae5461a7d82a301a22447269594e5b11763e27
itziarpb <itziar.perezb@gmail.com>
2022-10-19 18:46:12 +0000

    excusa

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My granma", "His turtle", "My bird"];
+  let whatDid = ["eat", "pissed", "crushed", "broked"];
+  let what = ["my homeworks", "the TV", "my stuff"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch"
+  ];
+  let num1 = Math.floor(who.length * Math.random());
+  let num2 = Math.floor(whatDid.length * Math.random());
+  let num3 = Math.floor(what.length * Math.random());
+  let num4 = Math.floor(when.length * Math.random());
+  return who[num1] + " " + whatDid[num2] + " " + what[num3] + " " + when[num4];
 };
